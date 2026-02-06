171cde67b6642e0127f63cea3a858059f0a4e99d
kleniolima <kleniorogerio.limapdita868@pditabira.com>
2026-02-06 06:31:48 -0300

    Add files via upload

---
+++ Modulo2/modulo2.2/aula2_questao1.py
@@ -0,0 +1,11 @@
+# aula2_questao1.py
+num1 = 5
+num2 = 2
+
+print("Tipo de num1:", type(num1))
+print("Tipo de num2:", type(num2))
+
+resultado = num1 / num2
+
+print("Resultado da divisão:", resultado)
+print("Tipo do resultado:", type(resultado))
