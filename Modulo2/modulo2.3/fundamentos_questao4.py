86f6b8b2a447c282a9a6d06e70e8a5011d853ecf
kleniolima <kleniorogerio.limapdita868@pditabira.com>
2026-02-06 07:40:30 -0300

    Add files via upload

---
+++ Modulo2/modulo2.3/fundamentos_questao4.py
@@ -0,0 +1,15 @@
+# Distância da maratona em quilômetros
+distancia_km = 42.195
+
+# Tempo total em horas
+tempo_horas = 3
+
+# Conversões
+distancia_metros = distancia_km * 1000
+tempo_segundos = tempo_horas * 3600
+
+# Cálculo da velocidade média
+velocidade_media = distancia_metros / tempo_segundos
+
+# Impressão do resultado
+print("Velocidade média necessária:", velocidade_media, "m/s")
