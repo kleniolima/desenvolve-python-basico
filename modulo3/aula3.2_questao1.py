52db14aaa78b4c58e40f896da077a67035fe75bf
kleniolima <kleniorogerio.limapdita868@pditabira.com>
2026-02-11 03:13:06 -0300

    Delete modulo3/aula3.2_questao1.py

--- modulo3/aula3.2_questao1.py
+++
@@ -1,8 +0,0 @@
-#idade de juliana 
-idade_juliana = int(input("digite a sua idade Juliana:"))
-
-#solicitar idade de Cris 
-idade_cris = int(input("Digite a sua idade Cris"))
-podem_entrar = idade_juliana > 17 and idade_cris > 17
-
-print (podem_entrar) 
