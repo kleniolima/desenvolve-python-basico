13ce18050304be9b3f6fc347c743260700985583
kleniolima <kleniorogerio.limapdita868@pditabira.com>
2026-09-07 16:26:19 -0300

    Add files via upload

---
+++ modulo5/aula1_questao1.py
@@ -0,0 +1,6 @@
+primeiro_numero = float(input("Digite o primeiro número: "))
+segundo_numero = float(input("Digite o segundo número: "))
+
+diferenca = round(abs(primeiro_numero - segundo_numero), 2)
+
+print(f"A diferença entre os números é: {diferenca:.2f}")
